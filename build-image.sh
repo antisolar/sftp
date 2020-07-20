557bf7e1ef0a38dbb74a4e6808fc3f3c9fbd962e
Adrian Dvergsdal <github.com@konto.atmoz.no>
2020-07-20 23:17:02 +0200

    Simpler build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-image_name="$1"
-image_version="$2"
-distro_name="$3"
-distro_version="$4"
-tags=("${@:5}")
-
-if [ "$distro_name" == "debian" ]; then
-    dockerfile="Dockerfile"
-else
-    dockerfile="Dockerfile-$distro_name"
-fi
+dockerfile="$1"
+tags=("${@:2}")
 
 tag_commands=()
 for tag in "${tags[@]}"; do
-    tag_commands+=("--tag=$image_name:$tag")
+    tag_commands+=("--tag=$tag")
 done
 
 docker build . \
-    --build-arg "distro_version=$distro_version" \
     --pull="true" \
     --file="$dockerfile" \
     "${tag_commands[@]}" \
